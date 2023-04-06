c563e856737d92aead5a7a6691fd6e87b8995ebc
Kavya-UK <102384284+Kavya-UK@users.noreply.github.com>
2023-04-06 19:11:11 +0530

    sample commit

--- src/Components/SidebarFooter.js
+++ src/Components/SidebarFooter.js
@@ -3,8 +3,10 @@ import React from 'react'
 export default function SidebarFooter() {
   return (
     <div class="px-14 pb-8 absolute inset-x-0 bottom-0  text-left">
+    
       <ul>
         <li class="py-2">
+    
           <a
             href="#"
             class="font-bold text-base leading-28 font-basic-sans  hover:text-sidebar_subTitleHover"
@@ -13,6 +15,7 @@ export default function SidebarFooter() {
           </a>
         </li>
         <li class="py-2">
+    
           <a
             href="#"
             class="font-bold text-base leading-28 font-basic-sans  text-left hover:text-sidebar_subTitleHover"
